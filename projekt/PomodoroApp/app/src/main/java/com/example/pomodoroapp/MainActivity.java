3896cc5c75129f0ed5044f986cae82a2f7a124b3
Sebastian Mielczarek <s.mielczarek@clearcode.cc>
2022-01-07 22:55:53 +0100

    Feature: starting page added

---
+++ projekt/PomodoroApp/app/src/main/java/com/example/pomodoroapp/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.pomodoroapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView welcome_icon;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void startIconAnimation(View view) {
+        welcome_icon = (ImageView) findViewById(R.id.welcome_icon);
+
+        Animation animZoomOut = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_out);
+        Animation animZoomIn = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
+        welcome_icon.startAnimation(animZoomIn);
+        animZoomIn.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                welcome_icon.startAnimation(animZoomOut);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+
+        }
+
+
+}
